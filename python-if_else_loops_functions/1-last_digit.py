62c5c71b055b3b55860daff1d4fbf20a9d5f7ae7
joevinM <joevin.manceau@hotmail.fr>
2025-09-02 15:39:05 +0200

    Add the script for the last digit task

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+modulo = number % 10 if number >= 0 else -(abs(number) % 10)
+print("Last digit of %d is %d" % (number, modulo), end=" ")
+if modulo > 5:
+    print("and is greater than 5")
+elif modulo == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
+    
