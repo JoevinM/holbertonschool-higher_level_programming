eea0cc257b2258f9764378c0891fbcb869eae5a4
joevinM <joevin.manceau@hotmail.fr>
2025-09-12 10:43:42 +0200

    Try to fix the task for the checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        compteur = 0
-        for i in range(0, x):
-            print(f"{my_list[i]}", end="")
+    compteur = 0
+    for i in range(0, x):
+        try:
+            print(my_list[i], end="")
             compteur += 1
-        print("")
-        return (compteur)
-    except IndexError:
-        print("")
-        return (compteur)
+        except IndexError:
+            break
+    print("")
+    return compteur
