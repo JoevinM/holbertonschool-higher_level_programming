d6c78b50ec60b1e77a5f5db2be6b39e05e981086
joevinM <joevin.manceau@hotmail.fr>
2025-09-12 10:32:16 +0200

    Try to fix the checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
         try:
             compteur = 0
-            for i in range(x):
+            for i in range(0, x):
                 print(f"{my_list[i]}", end="")
                 compteur += 1
             print()
