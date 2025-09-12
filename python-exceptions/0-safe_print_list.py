dd15fe6a26b2293cad7f14f719f9791501e4bc67
joevinM <joevin.manceau@hotmail.fr>
2025-09-12 10:37:35 +0200

    Try to fix for the checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             compteur += 1
         print("")
-        return compteur
+        return (compteur)
     except IndexError:
         print("")
-        return compteur
+        return (compteur)
