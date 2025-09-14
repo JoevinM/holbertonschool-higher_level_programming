1ab8151287d45ae0c9b82d9b72b8d16f80b4343d
joevinM <joevin.manceau@hotmail.fr>
2025-09-14 15:01:35 +0200

    Fix pycodestyle on task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,27 +4,32 @@
 Defines a function that divides all elements of a matrix.
 """
 
-def matrix_divided(matrix, div):
 
+def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by div and return the result
     """
 
-    if (not isinstance(matrix, list)  or
-        not all(isinstance(row, list) for row in matrix) or
-        not all(isinstance(num, (int, float)) for row in matrix for num in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if (not isinstance(matrix, list) or
+        not all(isinstance(
+            row, list) for row in matrix) or
+        not all(isinstance(
+            num, (int, float)) for row in matrix for num in row)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
     row_size = len(matrix[0])
     if any(len(row) != row_size for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    
+        raise TypeError(
+            "Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+        raise TypeError(
+            "div must be a number")
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError(
+            "division by zero")
 
     result = [[round(num / div, 2) for num in row] for row in matrix]
 
     return result
-    
