528fe5e588bbb798acee61572c767a68d02e783a
joevinM <joevin.manceau@hotmail.fr>
2025-09-15 15:00:48 +0200

    Add all files for the task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square that represents a geometric square.
+"""
+
+
+class Square:
+
+    """
+    This is an empty class that defines a square structure.
+    At this stage, the class does not have attributes or methods.
+    It only serves as a basic template for further development.
+    """
+
+    def __init__(self, size):
+
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
