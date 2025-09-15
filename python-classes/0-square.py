26e1799c42a3648b11cea3df9853f17ef4094936
joevinM <joevin.manceau@hotmail.fr>
2025-09-15 14:42:34 +0200

    Pycodestyle the script for the task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class Square that represents a geometric square.
+"""
+
 
 class Square:
+
+    """
+    This is an empty class that defines a square structure.
+    At this stage, the class does not have attributes or methods.
+    It only serves as a basic template for further development.
+    """
     pass
