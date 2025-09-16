8655555cfcb3c3197cb2d88681f584a36370b77b
joevinM <joevin.manceau@hotmail.fr>
2025-09-16 15:08:45 +0200

    Add all files and script for the task 0 and 1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Rectangle
+that represents a geometric rectangle.
+The Rectangle class currently serves as
+a basic template without attributes or methods.
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle structure.
+    At this stage, the class does not have attributes or methods.
+    It only serves as a basic template for further development.
+    """
+    pass
