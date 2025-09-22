308c0f38cc5b59a348f6f243b8bfbe0044d42bc5
joevinM <joevin.manceau@hotmail.fr>
2025-09-22 15:18:15 +0200

    Add all files for the task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry.
+This will serve as a base class for future geometry-related classes.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class.
+
+    Currently empty, but intended to be extended in the future
+    with geometry-related methods and attributes.
+    """
+    pass
