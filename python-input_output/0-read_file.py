61025fc152e7c0d2ef70248ae36d5a73f7d17da3
joevinM <joevin.manceau@hotmail.fr>
2025-09-29 10:58:20 +0200

    Fix checker

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -17,6 +17,6 @@ def read_file(filename=""):
         None
     """
 
-    with open('my_file_0.txt', "r", encoding="utf-8") as f:
+    with open('my_file_0.txt', mode="r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
