23c2e237adf62bbbcd5708929f4d28e09dc6b598
joevinM <joevin.manceau@hotmail.fr>
2025-09-29 15:14:47 +0200

    Fix pycodestyle

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,14 +11,13 @@ def save_to_json_file(my_obj, filename):
     Write a Python object to a file using JSON representation.
 
     Args:
-        my_obj: The Python object to serialize (e.g., dict, list, str, int, bool).
-        filename (str): The name of the file where the JSON string will be written.
+        my_obj: The Python object to serialize
+        (e.g., dict, list, str, int, bool).
+        filename (str): The name of the file where the
+        JSON string will be written.
 
     Returns:
         None
     """
-    # Open the file in write mode with UTF-8 encoding
-    # If the file exists, its content will be overwritten
-    with open(filename, "w", encoding="utf-8") as f:
-        # Convert the Python object into a JSON string and write it to the file
+    with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
