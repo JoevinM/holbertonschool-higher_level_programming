b720a2802fb8a06822aa01e1b30eb3c1b622318b
joevinM <joevin.manceau@hotmail.fr>
2025-09-30 10:29:14 +0200

    Fix files for the checker

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-
+"""
+Module that defines a function to load a Python object from a JSON file.
+"""
 
 import json
+
+
 def load_from_json_file(filename):
+     """
+    Lire un fichier JSON et le convertir en objet Python.
+
+    Args:
+        filename (str): le nom du fichier contenant les données JSON.
+
+    Returns:
+        object: l’objet Python obtenu après avoir décodé le contenu JSON.
+    """
     with open(filename) as f:
         return json.load(f)
