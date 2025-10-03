1808522181ea8f73dae9bab0f78521a781bbe71c
joevinM <joevin.manceau@hotmail.fr>
2025-10-03 10:48:54 +0200

    Add all files for the task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+Module to convert a CSV file into a JSON file.
+"""
+
+
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    """
+    Convert a CSV file into a JSON file with the same basename.
+
+    Args:
+        csv_file (str): Path to the CSV file.
+
+    Returns:
+        str: Path to the generated JSON file.
+    """
+
+    with open(csv_file, "r") as csvfile:
+        csv_data = csv.DictReader(csvfile)
+        with open("data_json", "w") as file_json:
+            list_conv = []
+            for row in csv_data:
+                list_conv.append(row)
+            json.dump(list_conv, file_json)
