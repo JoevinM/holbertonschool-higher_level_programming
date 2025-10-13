2d1a27442ef33881f2a8a61cd0744516d5d6ace6
joevinM <joevin.manceau@hotmail.fr>
2025-10-13 11:14:23 +0200

    Fix for the checker

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -46,7 +46,7 @@ class subclass(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"ok")
+            self.wfile.write(b"OK")
 
         elif self.path == '/info':
             self.send_response(200)
