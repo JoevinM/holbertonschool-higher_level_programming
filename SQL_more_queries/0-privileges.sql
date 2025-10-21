92618f8aa32f1fa35ec00921312ebbbb03a75aa4
joevinM <joevin.manceau@hotmail.fr>
2025-10-21 13:48:41 +0000

    Add script for the task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- That script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
