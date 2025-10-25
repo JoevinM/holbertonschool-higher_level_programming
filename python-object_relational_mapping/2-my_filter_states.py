151aa780fdd51dba66c28666c7710e0e6e1d1dfb
joevinM <joevin.manceau@hotmail.fr>
2025-10-25 16:40:21 +0200

    Try something with other code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,45 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+Script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa
 """
-
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    user = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
-        user=user,
-        passwd=password,
         port=3306,
-        db=db_name
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
 
+    # Create a cursor object
     cursor = db.cursor()
 
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".
-        format(state_name))
+    # Create SQL query using format with user input
+    query = "SELECT * FROM states WHERE name \
+            LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
 
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all results
     rows = cursor.fetchall()
 
+    # Display results
     for row in rows:
         print(row)
 
+    # Close cursor and connection
     cursor.close()
     db.close()
