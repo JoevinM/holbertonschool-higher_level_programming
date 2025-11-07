0f17b3fd938f1e83b8b9a6d141af064d8409945e
joevinM <joevin.manceau@hotmail.fr>
2025-11-07 15:23:57 +0100

    Fix error message

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < x; i++) {
     console.log('X'.repeat(x));
