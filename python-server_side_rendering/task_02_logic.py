35fba0b8923f619e3f22b1c32d29feb244f8a4b6
joevinM <joevin.manceau@hotmail.fr>
2025-11-14 16:53:02 +0100

    Add all files for the task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -16,6 +17,13 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    with open("items.json") as f:
+        data = json.load(f)
+    return render_template("items.html", items=data["items"])
 
 
 if __name__ == '__main__':
