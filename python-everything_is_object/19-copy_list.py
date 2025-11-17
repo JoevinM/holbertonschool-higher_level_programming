9ccfb05d1413ab83e04c8f730df4bc571bc58ca4
joevinM <joevin.manceau@hotmail.fr>
2025-11-17 11:21:51 +0100

    Pycodestyle for task 19

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,2 +1,8 @@
+#!/usr/bin/python3
+# Function that returns a copy of a list without using imports
+
 def copy_list(my_list):
+    # Using slicing ([:]) creates a new list with the same elements
+    # This ensures we return a real copy and not a reference
+
     return my_list[:]
